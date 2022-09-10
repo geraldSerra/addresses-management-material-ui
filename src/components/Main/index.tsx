1004361b0a8d317e39596d7bd6c128940f49d162
Gerald Serra Torres <geraldst20@gmail.com>
2022-09-10 19:21:32 -0400

    version 1.0.2

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,11 +1,9 @@
 import { Container } from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline';
 import Types from '../Typography';
 import Clients from '../Clients';
 
 const Main = () => (
   <Container>
-    <CssBaseline />
       <Types />
       <Clients />
   </Container>
